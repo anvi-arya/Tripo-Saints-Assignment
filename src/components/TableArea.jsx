6311b48c2a13811b10e38c17e324937cce881ed6
anvi-arya <anviarya846@gmail.com>
2023-03-07 18:35:15 +0530

    got table working : -bug: chair's overlapping

--- src/components/TableArea.jsx
+++ src/components/TableArea.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import TableData from "../data/TableData.json";
+import "./TableArea.css";
 
 export const TableArea = () => {
-  return <div>TableArea</div>;
+  return (
+    <div className="container">
+      {/* dynamically injecting tables */}
+      {TableData.map((table, indexTable) => {
+        return (
+          <div key={indexTable} className="table">
+            Table {indexTable + 1}
+            {table.map((chair, indexChair) => {
+              return (
+                <div key={indexChair}>
+                  <div
+                    className={`chair chair-${indexChair} ${
+                      chair !== null ? "chair-occupied" : ""
+                    }`}
+                  ></div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
